16bb85c4d5f20fd8de3585aff14f3693b9b8a5fc
Diego Tavares Volpatto <diego@esss.co>
2020-03-01 03:23:55 -0300

    Add docs to dict extension

--- python/reaktoro/__init__.py
+++ python/reaktoro/__init__.py
@@ -16,6 +16,13 @@ ChemicalOutput.array = lambda self: numpy.loadtxt(self.filename(), skiprows=1)
 # Define a method to convert the data in the file into a dictionary, with the 
 # headings being the keys, and the column data as the values.
 def _ChemicalOutput_dict(self):
+    """
+    Write ChemicalOutput results as a Python dictionary.
+
+    :return:
+        A python dict with select output properties.
+    :rtype dict:
+    """
     data = numpy.loadtxt(self.filename(), skiprows=1)
     columns = [data[:, i] for i in range(data.shape[1])]
     return {heading: column for heading, column in zip(self.headings(), columns)}
