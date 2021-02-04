fba8e389e4693411e836784d542fbb7474e409ff
Diego Volpatto <volpatto@lncc.br>
2021-02-03 23:07:53 -0300

    Add ex C1-CO2 from Pedersen, table 6.3

---
+++ tests/pedersen_63_fugacities.py
@@ -0,0 +1,169 @@
+import numpy as np
+import pandas as pd
+import pathlib
+
+import reaktoro
+
+import matplotlib.pyplot as plt
+
+
+def get_test_data_dir():
+    from pathlib import Path
+    import os
+    return Path(os.path.abspath(__file__)).parents[0] / "data"
+
+
+def _add_hydrocarbons_to_database(db):
+    hydrocarbons_db = reaktoro.Database(str(get_test_data_dir() / 'hydrocarbons.xml'))
+
+    element_names_in_db = set(e.name() for e in db.elements())
+    for element in hydrocarbons_db.elements():
+        if element.name() not in element_names_in_db:
+            db.addElement(element)
+
+    for species in hydrocarbons_db.gaseousSpecies():
+        db.addGaseousSpecies(species)
+
+    for species in hydrocarbons_db.liquidSpecies():
+        db.addLiquidSpecies(species)
+
+
+temperature = -42.0  # degC
+
+db = reaktoro.Database('supcrt07.xml')
+# _add_hydrocarbons_to_database(db)
+
+# db = reaktoro.Database(str(get_test_data_dir() / 'hydrocarbons.xml'))
+
+editor = reaktoro.ChemicalEditor(db)
+
+gaseous_species = ["CH4(g)", "CO2(g)"]
+oil_species = ["CH4(liq)", "CO2(liq)"]
+
+def calculate_bips(T):
+    k = np.zeros((2, 2))
+    k[0, 1] = 0.12
+    k[1, 0] = 0.12
+    bips = reaktoro.BinaryInteractionParams(k)
+    return bips
+
+eos_params = reaktoro.CubicEOSParams(
+    model=reaktoro.CubicEOSModel.SoaveRedlichKwong,
+    phase_identification_method=reaktoro.PhaseIdentificationMethod.GibbsEnergyAndEquationOfStateMethod,
+    binary_interaction_values=calculate_bips
+)
+
+editor.addGaseousPhase(gaseous_species).setChemicalModelCubicEOS(eos_params)
+editor.addLiquidPhase(oil_species).setChemicalModelCubicEOS(eos_params)
+
+system = reaktoro.ChemicalSystem(editor)
+
+state = reaktoro.ChemicalState(system)
+state.setTemperature(temperature, 'degC')
+
+## Gathering pvtlib results ##
+# Fugacities
+data_dir = pathlib.Path(__file__).parent.absolute() / "data"
+df_pvtlib_fugacities = pd.read_excel(data_dir / "pvtlib-pedersen63-flash-analysis.xlsx", sheet_name="Phase Fugacities")
+
+# Composition
+df_pvtlib_compositions = pd.read_excel(data_dir / "pvtlib-pedersen63-flash-analysis.xlsx", sheet_name="Phase Composition")
+
+# Normalizing fugacities
+P_1_atm_as_Pa = 101325.0
+df_pvtlib_fugacities["C1"] = df_pvtlib_fugacities["C1"] / P_1_atm_as_Pa
+df_pvtlib_fugacities["CO2"] = df_pvtlib_fugacities["CO2"] / P_1_atm_as_Pa
+
+# Retrieve results by phase
+df_pvtlib_fugacities_gas = df_pvtlib_fugacities[df_pvtlib_fugacities.Phase == 1]
+df_pvtlib_fugacities_gas.reset_index(drop=True, inplace=True)
+df_pvtlib_fugacities_liq = df_pvtlib_fugacities[df_pvtlib_fugacities.Phase == 2]
+df_pvtlib_fugacities_liq.reset_index(drop=True, inplace=True)
+df_pvtlib_composition_gas = df_pvtlib_compositions[df_pvtlib_compositions.Phase == 1]
+df_pvtlib_composition_gas.reset_index(drop=True, inplace=True)
+df_pvtlib_composition_liq = df_pvtlib_compositions[df_pvtlib_compositions.Phase == 2]
+df_pvtlib_composition_liq.reset_index(drop=True, inplace=True)
+
+# Properties' calculation loop for gas phase
+activities_gas = list()
+pvtlib_fugacities_gas = list()
+for index, df_row in df_pvtlib_composition_gas.iterrows():
+    P = df_row["Pressure [Pa]"]
+    composition_0 = df_row["C1"]
+    composition_1 = df_row["CO2"]
+    state.setPressure(P, 'Pa')
+    state.setSpeciesAmount(gaseous_species[0], composition_0)
+    state.setSpeciesAmount(gaseous_species[1], composition_1)
+
+    df_row_fugacities = df_pvtlib_fugacities_gas.iloc[index, :]
+    fugacity_0 = df_row_fugacities["C1"]
+    fugacity_1 = df_row_fugacities["CO2"]
+    fugacities = np.array([fugacity_0, fugacity_1])
+
+    gas_properties = state.properties()
+    activities = np.exp(gas_properties.lnActivities().val)
+    activities_gas.append(activities)
+    pvtlib_fugacities_gas.append(fugacities)
+
+activities_gas = np.array(activities_gas)
+pvtlib_fugacities_gas = np.array(pvtlib_fugacities_gas)
+pressure_values = df_pvtlib_composition_gas["Pressure [Pa]"].values / 100000  # to bar
+
+plt.figure(figsize=(8, 6))
+
+plt.plot(pressure_values, activities_gas[:, 0], "-x", label="C1(g) - Reaktoro")
+plt.plot(pressure_values, pvtlib_fugacities_gas[:, 0], "-o", label="C1(g) - pvtlib")
+plt.plot(pressure_values, activities_gas[:, 1], "-x", label="CO2(g) - Reaktoro")
+plt.plot(pressure_values, pvtlib_fugacities_gas[:, 1], "-o", label="CO2(g) - pvtlib")
+
+plt.xlabel("Pressure [bar]")
+plt.ylabel("Fugacities [bar]")
+plt.title(f"Fixed T = {temperature} degC")
+plt.legend(shadow=True)
+
+plt.grid(True)
+
+plt.savefig("reaktoro_pvtlib_fugacities_gas_pedersen.png", dpi=300)
+plt.show()
+
+# Properties' calculation loop for gas phase
+activities_liq = list()
+pvtlib_fugacities_liq = list()
+for index, df_row in df_pvtlib_composition_liq.iterrows():
+    P = df_row["Pressure [Pa]"]
+    composition_0 = df_row["C1"]
+    composition_1 = df_row["CO2"]
+    state.setPressure(P, 'Pa')
+    state.setSpeciesAmount(oil_species[0], composition_0)
+    state.setSpeciesAmount(oil_species[1], composition_1)
+
+    df_row_fugacities = df_pvtlib_fugacities_liq.iloc[index, :]
+    fugacity_0 = df_row_fugacities["C1"]
+    fugacity_1 = df_row_fugacities["CO2"]
+    fugacities = np.array([fugacity_0, fugacity_1])
+
+    liq_properties = state.properties()
+    activities = np.exp(liq_properties.lnActivities().val)
+    activities_liq.append(activities)
+    pvtlib_fugacities_liq.append(fugacities)
+
+activities_liq = np.array(activities_liq)
+pvtlib_fugacities_liq = np.array(pvtlib_fugacities_liq)
+pressure_values = df_pvtlib_composition_liq["Pressure [Pa]"].values / 100000  # to bar
+
+plt.figure(figsize=(8, 6))
+
+plt.plot(pressure_values, activities_liq[:, 0], "-x", label="C1(liq) - Reaktoro")
+plt.plot(pressure_values, pvtlib_fugacities_liq[:, 0], "-o", label="C1(liq) - pvtlib")
+plt.plot(pressure_values, activities_liq[:, 1], "-x", label="CO2(liq) - Reaktoro")
+plt.plot(pressure_values, pvtlib_fugacities_liq[:, 1], "-o", label="CO2(liq) - pvtlib")
+
+plt.xlabel("Pressure [bar]")
+plt.ylabel("Fugacities [bar]")
+plt.title(f"Fixed T = {temperature} degC")
+plt.legend(shadow=True)
+
+plt.grid(True)
+
+plt.savefig("reaktoro_pvtlib_fugacities_liq_pedersen.png", dpi=300)
+plt.show()
---
+++ tests/pedersen_63_previous_sol.py
@@ -0,0 +1,134 @@
+import numpy as np
+import pandas as pd
+import pathlib
+
+import reaktoro
+
+import matplotlib.pyplot as plt
+
+
+def get_test_data_dir():
+    from pathlib import Path
+    import os
+    return Path(os.path.abspath(__file__)).parents[0] / "data"
+
+
+def _add_hydrocarbons_to_database(db):
+    hydrocarbons_db = reaktoro.Database(str(get_test_data_dir() / 'hydrocarbons.xml'))
+
+    element_names_in_db = set(e.name() for e in db.elements())
+    for element in hydrocarbons_db.elements():
+        if element.name() not in element_names_in_db:
+            db.addElement(element)
+
+    for species in hydrocarbons_db.gaseousSpecies():
+        db.addGaseousSpecies(species)
+
+    for species in hydrocarbons_db.liquidSpecies():
+        db.addLiquidSpecies(species)
+
+
+temperature = -42.0  # degC
+
+db = reaktoro.Database('supcrt98.xml')
+# _add_hydrocarbons_to_database(db)
+
+# db = reaktoro.Database(str(get_test_data_dir() / 'hydrocarbons.xml'))
+
+editor = reaktoro.ChemicalEditor(db)
+
+gaseous_species = ["CH4(g)", "CO2(g)"]
+oil_species = ["CH4(liq)", "CO2(liq)"]
+composition = np.array([0.40, 0.60])
+
+def calculate_bips(T):
+    k = np.zeros((2, 2))
+    k[0, 1] = 0.12
+    k[1, 0] = 0.12
+    bips = reaktoro.BinaryInteractionParams(k)
+    return bips
+
+eos_params = reaktoro.CubicEOSParams(
+    model=reaktoro.CubicEOSModel.SoaveRedlichKwong,
+    phase_identification_method=reaktoro.PhaseIdentificationMethod.GibbsEnergyAndEquationOfStateMethod,
+    binary_interaction_values=calculate_bips
+)
+
+editor.addGaseousPhase(gaseous_species).setChemicalModelCubicEOS(eos_params)
+editor.addLiquidPhase(oil_species).setChemicalModelCubicEOS(eos_params)
+
+system = reaktoro.ChemicalSystem(editor)
+problem = reaktoro.EquilibriumProblem(system)
+
+problem.setTemperature(temperature, 'degC')
+problem.add('CH4', composition[0], "mol")
+problem.add('CO2', composition[1], "mol")
+
+options = reaktoro.EquilibriumOptions()
+options.hessian = reaktoro.GibbsHessian.Exact  # required change for this case
+
+solver = reaktoro.EquilibriumSolver(system)
+solver.setOptions(options)
+
+# In this case, we should provide a better initial guess instead of the default.
+state = reaktoro.ChemicalState(system)
+state.setSpeciesAmounts(0.001)
+state.setSpeciesAmount("CH4(g)", composition[0])
+state.setSpeciesAmount("CO2(g)", composition[1])
+
+# Retrieve pvtlib solution
+data_dir = pathlib.Path(__file__).parent.absolute() / "data"
+df_pvtlib_result = pd.read_excel(
+    data_dir / "pvtlib-pedersen63-flash-analysis.xlsx", 
+    sheet_name="Phase Composition"
+)
+
+pvtlib_phase_fractions_gas = df_pvtlib_result[df_pvtlib_result.Phase == 1]["Molar Fraction"].values
+pvtlib_pressure_values_gas = df_pvtlib_result[df_pvtlib_result.Phase == 1]["Pressure [Pa]"].values
+pvtlib_pressure_values_gas = pvtlib_pressure_values_gas / 100000  # to bar
+# pvtlib_pressure_values_gas.reset_index(drop=True, inplace=True)
+
+pvtlib_phase_fractions_liquid = df_pvtlib_result[df_pvtlib_result.Phase == 2]["Molar Fraction"].values
+pvtlib_pressure_values_liquid = df_pvtlib_result[df_pvtlib_result.Phase == 2]["Pressure [Pa]"].values
+pvtlib_pressure_values_liquid = pvtlib_pressure_values_liquid / 100000  # to bar
+# pvtlib_pressure_values_liquid.reset_index(drop=True, inplace=True)
+
+pressure_values = np.linspace(10, 75, 200)
+phase_fractions_liquid = list()
+phase_fractions_gas = list()
+pressure_values_converged = list()
+for P in pressure_values:
+    problem.setPressure(P, 'bar')
+    has_converged = solver.solve(state, problem)
+    print(f"P = {P} bar; T = {temperature} degC; Converged? {has_converged.optimum.succeeded}")
+
+    molar_base = state.phaseAmount('Gaseous') + state.phaseAmount('Liquid')
+
+    gas_phase_molar_fraction = state.phaseAmount('Gaseous') / molar_base
+    phase_fractions_gas.append(gas_phase_molar_fraction)
+
+    liquid_phase_molar_fraction = state.phaseAmount('Liquid') / molar_base
+    phase_fractions_liquid.append(liquid_phase_molar_fraction)
+
+    pressure_values_converged.append(P)
+
+phase_fractions_gas = np.array(phase_fractions_gas)
+phase_fractions_liquid = np.array(phase_fractions_liquid)
+pressure_values_converged = np.array(pressure_values_converged)
+
+plt.figure(figsize=(8, 6))
+plt.plot(pressure_values_converged, phase_fractions_liquid, "-x", label="Liquid (Reaktoro)")
+plt.plot(pvtlib_pressure_values_liquid, pvtlib_phase_fractions_liquid, "-x", label="Liquid (pvtlib)")
+
+plt.plot(pressure_values_converged, phase_fractions_gas, "-o", label="Gas (Reaktoro)")
+plt.plot(pvtlib_pressure_values_gas, pvtlib_phase_fractions_gas, "-o", label="Gas (pvtlib)")
+
+plt.xlabel("Pressure [bar]")
+plt.ylabel("Phase molar fraction [mol / mol]")
+plt.title(f"Fixed T = {temperature} degC")
+plt.legend(shadow=True)
+
+plt.grid(True)
+
+plt.savefig("reaktoro_pvtlib_using_prev_pedersen.png", dpi=300)
+plt.show()
